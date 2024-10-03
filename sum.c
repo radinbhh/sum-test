bca2d660afe6bfb4167737d3b6ed6824d5bcd239
radin baharsefat <60336925+radinbhh@users.noreply.github.com>
2024-10-03 16:40:01 +0330

    added mult

--- sum.c
+++ sum.c
@@ -1,13 +1,24 @@
 #include<stdio.h>
 int main()
 {
+	int what;
+	printf("enter 0 for sum and 1 for multiply:");
+	scanf("%d",&what);
 	int a,b;
 	printf("enter a:");
 	scanf("%d",&a);
 	printf("\nenter b:");
 	scanf("%d",&b);
 	printf("\n");
+	if (what == 0)
+	{
 	printf("%d",a+b);
+	}
+	else
+	{
+		printf("%d",a*b);
+		
+	}
 	printf("\n");
 	return 0;
 }
